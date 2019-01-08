552ea87c7aba265911d6df70d9c11844fe1ec1e0
Brendlu16 <brendlu16@sps-prosek.cz>
2019-01-08 09:57:38 +0100

    vse

---
+++ Launcher/Generator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Brushes = System.Windows.Media.Brushes;
+using Image = System.Windows.Controls.Image;
+
+namespace Launcher
+{
+    class Generator
+    {
+        public static Grid GenerovatGridSButtonem(int i, string subitem)
+        {
+            Button newBtn = new Button
+            {
+                Name = "Button" + i.ToString(),
+                BorderThickness = new Thickness(0),
+                Background = Brushes.Transparent
+            };
+            newBtn.Click += new RoutedEventHandler(MainWindow.ButtonClick);
+            Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(subitem);
+            var bitmap = icon.ToBitmap();
+            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            StackPanel newStackPanel = new StackPanel
+            {
+                Width = 90,
+                Height = 60
+            };
+            Image newImage = new Image
+            {
+                Source = bitmapSource,
+                Width = 40,
+                Height = 40,
+                HorizontalAlignment = HorizontalAlignment.Stretch
+            };
+            Label newLabel = new Label
+            {
+                Content = Path.GetFileNameWithoutExtension(subitem),
+                Width = 90,
+                Height = 30,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                HorizontalContentAlignment = HorizontalAlignment.Center
+            };
+            Grid newGrid = new Grid();
+            Button newBtn2 = new Button
+            {
+                Name = "Button2" + i.ToString(),
+                BorderThickness = new Thickness(0),
+                Background = Brushes.Transparent,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Bottom,
+                Height = 20,
+                Width = 20
+            };
+            newBtn2.Click += new RoutedEventHandler(MainWindow.TestBtn_Click);
+            ContextMenu newContextMenu = new ContextMenu
+            {
+                Background = Brushes.White,
+                HasDropShadow = false,
+                BorderThickness = new Thickness(0)
+            };
+            MenuItem newMenuItem = new MenuItem
+            {
+                Name = "MenuItem1" + i.ToString(),
+                Header = "Otevřít umístění"
+            };
+            newMenuItem.Click += new RoutedEventHandler(MainWindow.MenuItem1Click);
+            MenuItem newMenuItem2 = new MenuItem
+            {
+                Name = "MenuItem2" + i.ToString(),
+                Header = "Otevřít umístění projektu"
+            };
+            Image newImage2 = new Image
+            {
+                Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory+@"\imgs\menu_lines.png"))
+            };
+            newMenuItem2.Click += new RoutedEventHandler(MainWindow.MenuItem2Click);
+            newContextMenu.Items.Add(newMenuItem);
+            newContextMenu.Items.Add(newMenuItem2);
+            newBtn2.ContextMenu = newContextMenu;
+            newBtn2.Content = newImage2;
+            newStackPanel.Children.Add(newImage);
+            newStackPanel.Children.Add(newLabel);
+            newBtn.Content = newStackPanel;
+            newGrid.Children.Add(newBtn);
+            newGrid.Children.Add(newBtn2);
+            return newGrid;
+        }
+    }
+}
---
+++ Launcher/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using Path = System.IO.Path;
+using System.Windows.Interop;
+using Image = System.Windows.Controls.Image;
+using Brushes = System.Windows.Media.Brushes;
+
+namespace Launcher
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<string> exefiles = new List<string>();
+        static string path;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Vypis();
+        }
+        public void Vypis()
+        {
+            try
+            {
+                string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                string usrname = userName.Substring(userName.IndexOf(@"\")); // D:\username neexistuje
+                path = @"D:\source\repos";//+usrname;
+                int i = 0;
+                string[] files = Directory.GetFiles(path, "*.sln", SearchOption.AllDirectories);
+                foreach (string item in files)
+                {
+                    string subpath = Path.GetDirectoryName(item);
+                    string[] subfiles = Directory.GetFiles(subpath, "*.exe", SearchOption.AllDirectories);
+                    foreach (string subitem in subfiles)
+                    {
+                        if (Path.GetFileNameWithoutExtension(subitem) == Path.GetFileNameWithoutExtension(item) && subitem.Contains("bin"))
+                        {
+                            PridatButton(i, subitem);
+                            i++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Error.Content = ex.Message;
+            }
+        }
+        public void PridatButton(int i, string subitem)
+        {
+            Grid newGrid = Generator.GenerovatGridSButtonem(i, subitem);
+            exefiles.Add(subitem);
+            Wrap.Children.Add(newGrid);
+        }
+        public static void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            string nazev = button.Name.ToString();
+            int execislo = int.Parse(nazev.Remove(0, 6));
+            Process proc = new Process();
+            proc.StartInfo.FileName = Path.GetFileName(exefiles[execislo]);
+            proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(exefiles[execislo]);
+            proc.Start();
+        }
+
+        public static void TestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button Btn = sender as Button;
+            ContextMenu contextMenu = Btn.ContextMenu;
+            contextMenu.PlacementTarget = Btn;
+            contextMenu.IsOpen = true;
+        }
+
+        public static void MenuItem1Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            string nazev = item.Name;
+            int execislo = int.Parse(nazev.Remove(0, 9));
+            Process.Start("explorer.exe", Path.GetDirectoryName(exefiles[execislo]));
+        }
+        public static void MenuItem2Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            string nazev = item.Name;
+            int execislo = int.Parse(nazev.Remove(0, 9));
+            string nazevsouboru = Path.GetFileNameWithoutExtension(exefiles[execislo]);
+            string[] files = Directory.GetFiles(path, nazevsouboru+".sln", SearchOption.AllDirectories);
+            Process.Start("explorer.exe", Path.GetDirectoryName(files[0]));
+        }
+    }
+}
